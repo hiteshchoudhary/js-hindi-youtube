4987ecedc5d1cae9961a54cf1c229402df763426
Hitesh Choudhary <hitesh@hiteshchoudhary.com>
2023-02-25 10:00:30 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,34 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
