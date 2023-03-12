d209268b7603f8d1dc60436e76d663f6f840ff73
Hitesh Choudhary <hitesh@hiteshchoudhary.com>
2023-03-12 12:03:53 +0000

    Array part 1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,11 +1,10 @@
+// array
+
 const myArr = [0, 1, 2, 3, 4, 5]
-const myHeros = ["shaktiman", "naagraj"]
-const myArr2 = new Array(1, 2, 3, 4, 5)
+const myHeors = ["shaktiman", "naagraj"]
 
-// access the array value
-// console.log(myArr[1] + myArr2[3]);
-
-console.log(myArr.length);
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
 
 // Array methods
 
@@ -13,24 +12,28 @@ console.log(myArr.length);
 // myArr.push(7)
 // myArr.pop()
 
-// myArr.unshift(0)
+// myArr.unshift(9)
 // myArr.shift()
-// console.log(myArr.reverse());
 
-// console.log(myArr.includes("3"));
+// console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
-const newArr = myArr.join()
-console.log(typeof newArr);
 
-// slice splice
+// const newArr = myArr.join()
 
-console.log("A", myArr);
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3)
+
 console.log(myn1);
-console.log("B", myArr);
+console.log("B ", myArr);
+
 
 const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
 console.log(myn2);
-
-console.log("C", myArr);
