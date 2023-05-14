3c5dde1deb58ab45f14105d35b20e64c22aa3a1b
Kishan Aher <kishanaher12@gmail.com>
2023-05-14 10:19:16 +0530

    basic of function call by IIFE --completed

---
+++ 03_basics/03_IIFE.js
@@ -0,0 +1,9 @@
+// const chai=()=>{
+//     console.log("hello");
+// };
+// chai();
+
+//Invoke immediate function Expression
+(function chai(name="yashwant"){
+    console.log(`${name} welcome to IIFE function`);
+})("kishan");
