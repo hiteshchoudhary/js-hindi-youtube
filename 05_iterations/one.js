11596d4425e615fa2ae020967369b35bd20031c1
Hitesh Choudhary <hitesh@hiteshchoudhary.com>
2023-05-21 18:19:22 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,49 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        //console.log("5 is best number");
+    }
+    //console.log(element);
+    
+}
+
+// console.log(element);
+
+for (let i = 1; i <= 10; i++) {
+    //console.log(`Outer loop value: ${i}`);
+   for (let j = 1; j <= 10; j++) {
+    //console.log(`Inner loop value ${j} and inner loop ${i}`);
+    //console.log(i + '*' + j + ' = ' + i*j );
+   }
+    
+}
+let myArray = ["flash", "batman", "superman"]
+//console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element);
+    
+}
+
+
+// break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+//    console.log(`Value of i is ${index}`);
+    
+// }
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+   console.log(`Value of i is ${index}`);
+    
+}
